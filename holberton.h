d63d6612c7e39d8760358d9441b0322dad1651ec
Larry Madeo <lmadeo76@gmail.com>
2017-07-10 23:08:40 +0000

    first commit of header file

---
+++ holberton.h
@@ -0,0 +1,19 @@
+#ifndef HOLBER_H
+#define HOLBER_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/* helper_functions.c */
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+/* helper2.c */
+char *_strdup(char *str);
+
+#endif /* HOLBER_H */
