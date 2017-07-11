68c81165d16520ac85d140ee303a3ad2f7fb9d75
Larry Madeo <lmadeo76@gmail.com>
2017-07-11 23:38:33 +0000

    prototypes of init_buffer functions added

--- holberton.h
+++ holberton.h
@@ -9,6 +9,7 @@
 #include <unistd.h>
 
 #define UNUSED(x) (void)(x)
+#define BUFSIZE 1024
 
 /* helper_functions.c */
 int _strcmp(char *s1, char *s2);
@@ -19,5 +20,7 @@ int _strlen(char *s);
 /* helper2.c */
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int init_char_buffer(char *buffer, int bufsize);
+int init_Cptr_buffer(char **buffer, int bufsize);
 
 #endif /* HOLBER_H */
